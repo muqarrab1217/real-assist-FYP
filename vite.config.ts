a5d5a3fed54ece30364929caf0d4a48f8db399a0
Muqarrab Ahmed <muq1217@gmail.com>
2025-10-13 00:20:19 +0500

    updated port requests for render

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
+  },
+  server: {
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: ['real-assist-fyp.onrender.com'],
   },
 })
